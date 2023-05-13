d53fd6b1412951b671a18e254aaf95abc5aa8d4b
Isaka Developer <isaxstarisaka@gmail.com>
2023-05-14 00:18:11 +0300

    first commit

---
+++ Flash Chat/Controllers/ChatViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+}
---
+++ Flash Chat/Controllers/LoginViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+}
